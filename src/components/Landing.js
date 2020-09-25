e982df8a142566afefc0625c728d22b63cf4b465
Lee Castelani <notayogilee@gmail.com>
2020-09-25 11:09:30 -0400

    first two sections done

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,15 +1,21 @@
-import React, { useState, Fragment } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
 import './Landing.scss';
 import Navbar from './Navbar';
 import champ from '../img/01_BOLDWIN_Champ_D_Eau.png';
 import roses from '../img/02_BOLDWIN_Massif_Des_Roses.png';
 import pecheur from '../img/03_BOLDWIN_Pecheur.png';
-import nord from '../img/04_BOLDWIN_Nord_Amerique.png';
+import nord from '../img/04_BOLDWIN_Nord_Amerique_Petit.png';
 import caux from '../img/05_BOLDWIN_Caux.png';
 import fleuve from '../img/06_BOLDWIN_Fleuve.png';
 import saison from '../img/08_BOLDWIN_La_Saison_Est_Courte.png';
 
 const Landing = () => {
+  useEffect(() => {
+    M.AutoInit();
+  });
+
 
   const [intro, setIntro] = useState(false);
 
@@ -29,36 +35,23 @@ const Landing = () => {
         <Fragment>
           <Navbar />
           <div id="main">
-            <h1 id="slide-right">Signalétique — RIDM 2019</h1>
+            <h4 id="slide-right">Signalétique — RIDM 2019</h4>
             <p id="slide-left">
               Réserve et classicisme caractérisent le langage signalétique que nous avons désigné pour l’édition 2019 des Rencontres internationales du documentaire de Montréal, une sobriété qui entre en cohérence avec celle de la Cinémathèque québécoise.
           </p>
           </div>
           <div id="beer">
-            <div class="carousel">
-              <a class="carousel-item" href="#one!"><img src={champ} /></a>
-              <a class="carousel-item" href="#two!"><img src={roses} /></a>
-              <a class="carousel-item" href="#three!"><img src={pecheur} /></a>
-              <a class="carousel-item" href="#four!"><img src={nord} /></a>
-              <a class="carousel-item" href="#five!"><img src={caux} /></a>
-              <a class="carousel-item" href="#six!"><img src={fleuve} /></a>
-              <a class="carousel-item" href="#seven!"><img src={saison} /></a>
-              <a class="carousel-item" href="#eight!"><div class="card">
-                <div class="card-image waves-effect waves-block waves-light">
-                  <img class="activator" src={saison} />
-                </div>
-                <div class="card-content">
-                  <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
-                  <p><a href="#">This is a link</a></p>
-                </div>
-                <div class="card-reveal">
-                  <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
-                  <p>Here is some more information about this product that is only revealed once clicked on.</p>
-                </div>
-              </div></a>
+            <div className="carousel">
+              <a className="carousel-item" href="#one!"><img src={champ} /></a>
+              <a className="carousel-item" href="#two!"><img src={roses} /></a>
+              <a className="carousel-item" href="#three!"><img src={pecheur} /></a>
+              <a className="carousel-item" href="#four!"><img src={nord} /></a>
+              <a className="carousel-item" href="#five!"><img src={caux} /></a>
+              <a className="carousel-item" href="#six!"><img src={fleuve} /></a>
+              <a className="carousel-item" href="#seven!"><img src={saison} /></a>
             </div>
-            <h1 id="slide-right">Articulation visuelle — Boldwin</h1>
-            <p id="slide-left">
+            <h2 className="beer-title">Articulation visuelle — Boldwin</h2>
+            <p className="beer-description">
               Le succès retentissant de l’identité que nous avons développé pour les bières biologiques Boldwin nous a conduit à déployer son langage sur tout un essaim de canevas. Allant de la création de contenu rédactionnel, à l’organisation rythmique de leurs nouveaux signes sur des affiches, des balluchons et des paquetages. Nous constatons avec plaisir que le symbolisme du territoire québécois mobilise l’intérêt.
           </p>
           </div>
