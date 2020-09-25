409d533412184353693c3e40ec6aa87d86a92b9c
Lee Castelani <notayogilee@gmail.com>
2020-09-25 12:25:28 -0400

    add background video for alphabet section

---
+++ src/components/Alphabet.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Alphabet = () => {
+  return (
+    <div>
+
+    </div>
+  )
+};
+
+export default Alphabet;
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -6,10 +6,11 @@ import Navbar from './Navbar';
 import champ from '../img/01_BOLDWIN_Champ_D_Eau.png';
 import roses from '../img/02_BOLDWIN_Massif_Des_Roses.png';
 import pecheur from '../img/03_BOLDWIN_Pecheur.png';
-import nord from '../img/04_BOLDWIN_Nord_Amerique_Petit.png';
+import nord from '../img/04_BOLDWIN_Nord_Amerique.png';
 import caux from '../img/05_BOLDWIN_Caux.png';
 import fleuve from '../img/06_BOLDWIN_Fleuve.png';
 import saison from '../img/08_BOLDWIN_La_Saison_Est_Courte.png';
+import alphabetVideo from '../img/alphabet.mp4';
 
 const Landing = () => {
   useEffect(() => {
@@ -17,7 +18,7 @@ const Landing = () => {
   });
 
 
-  const [intro, setIntro] = useState(false);
+  const [intro, setIntro] = useState(true);
 
   setTimeout(() => {
     setIntro(false)
@@ -38,8 +39,9 @@ const Landing = () => {
             <h4 id="slide-right">Signalétique — RIDM 2019</h4>
             <p id="slide-left">
               Réserve et classicisme caractérisent le langage signalétique que nous avons désigné pour l’édition 2019 des Rencontres internationales du documentaire de Montréal, une sobriété qui entre en cohérence avec celle de la Cinémathèque québécoise.
-          </p>
+            </p>
           </div>
+
           <div id="beer">
             <div className="carousel">
               <a className="carousel-item" href="#one!"><img src={champ} /></a>
@@ -54,6 +56,12 @@ const Landing = () => {
             <p className="beer-description">
               Le succès retentissant de l’identité que nous avons développé pour les bières biologiques Boldwin nous a conduit à déployer son langage sur tout un essaim de canevas. Allant de la création de contenu rédactionnel, à l’organisation rythmique de leurs nouveaux signes sur des affiches, des balluchons et des paquetages. Nous constatons avec plaisir que le symbolisme du territoire québécois mobilise l’intérêt.
           </p>
+          </div>
+
+          <div id="alphabet">
+            <video id="alphabet-video" autoPlay loop muted >
+              <source src={alphabetVideo} type="video/mp4" />
+            </video>
           </div>
 
         </Fragment>
